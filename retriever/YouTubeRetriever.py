55a0d966ea28d94e921f681a924bae8e7981cd1b
xro7 <xro7nis@gmail.com>
2018-11-26 22:42:52 +0200

    added search term functionality for multiple terms

--- retriever/YouTubeRetriever.py
+++ retriever/YouTubeRetriever.py
@@ -47,7 +47,7 @@ class YouTubeRetriever:
 
         return subscriptions_ids
 
-    def get_video_ids_from_channel_ids(self, channel_ids, return_list=True,verbose=False):
+    def get_video_ids_from_channel_ids(self, channel_ids, return_list=True, verbose=False):
         # https: // developers.google.com / youtube / v3 / docs / search / list
         if verbose:
             print('Retrieving video ids for every one of {} channel ids...'.format(len(channel_ids)))
@@ -59,7 +59,7 @@ class YouTubeRetriever:
             while 'nextPageToken' in response:
                 try:
                     response = self.API_connection.search().list(
-                        part='id', channelId=channel_id, maxResults=50,pageToken=response['nextPageToken']).execute()
+                        part='id', channelId=channel_id, maxResults=50, pageToken=response['nextPageToken']).execute()
                 except HttpError as e:
                     print(e, channel_id)
                     break
@@ -67,7 +67,8 @@ class YouTubeRetriever:
                     if 'videoId' in item['id']:
                         videos[channel_id].append(item['id']['videoId'])
             if verbose:
-                print('{}. {} video ids were retrieved for channel id: {}'.format(i,len(videos[channel_id]),channel_id))
+                print('{}. {} video ids were retrieved for channel id: {}'.
+                      format(i,len(videos[channel_id]), channel_id))
         if return_list:
             videos_list = []
             for k, v in videos.items():
@@ -76,7 +77,48 @@ class YouTubeRetriever:
         else:
             return videos
 
-    def get_comments_from_video_ids(self, video_ids, max_comments_per_video=None, return_list=True,verbose=False):
+    def get_comments(self, video_id, max_comments_per_video, order, search_term=None, return_ids_dict=False):
+        comment_dict = dict()
+        all_comments = []
+        response = dict()
+        response['nextPageToken'] = ''
+        while 'nextPageToken' in response:
+            try:
+                response = self.API_connection.commentThreads().list(
+                    part='snippet,replies', videoId=video_id, maxResults=100,
+                    order=order, textFormat='plainText', searchTerms=search_term, pageToken=response['nextPageToken']).execute()
+            except HttpError as e:
+                print(e, video_id)
+                break
+            comments_for_video = []
+            for item in response['items']:
+                comment = item["snippet"]["topLevelComment"]
+                comment_id = comment['id']
+                # author = comment["snippet"]["authorDisplayName"]
+                text = comment["snippet"]["textOriginal"]
+                comments_for_video.append(text)
+                comment_dict[comment_id] = text
+                if 'replies' in item:
+                    replies = item['replies']['comments']
+                    for reply in replies:
+                        comment_id = reply['id']
+                        text = reply["snippet"]["textOriginal"]
+                        comments_for_video.append(text)
+                        comment_dict[comment_id] = text
+            all_comments.extend(comments_for_video)
+            if max_comments_per_video is not None:
+                if len(comment_dict) >= max_comments_per_video:
+                    #all_comments = all_comments[:max_comments_per_video]
+                    comment_dict = {k: comment_dict[k] for k in list(comment_dict)[:max_comments_per_video]}
+                    all_comments = comment_dict.values()
+                    break
+        if return_ids_dict:
+            return comment_dict
+        else:
+            return all_comments
+
+    def get_comments_from_video_ids(self, video_ids, max_comments_per_video=None,
+                                    order='relevance', search_terms=None, return_list=True, verbose=False):
 
         # https: // developers.google.com / youtube / v3 / docs / commentThreads / list
         # requests_per_video: each requests returns at most 100 parent comments alongside with some of their replies
@@ -85,38 +127,30 @@ class YouTubeRetriever:
         # and you need to use the comments.list method if you want to retrieve all replies for a particular comment.)
         # ---> https: // developers.google.com / youtube / v3 / guides / implementation / comments
 
+        # seatchTerms attribute: there is a boolean and condintion between terms. So if we want or condition we need
+        # to make multiple requests
+        assert order == 'relevance' or order == 'time'
+
         if verbose:
             print('Retrieving comments for every one of {} video ids...'.format(len(video_ids)))
         comments = dict()
 
         for i, video_id in enumerate(video_ids):
-            comments[video_id] = []
-            response = dict()
-            response['nextPageToken'] = ''
-            while 'nextPageToken' in response:
-                try:
-                    response = self.API_connection.commentThreads().list(
-                        part='snippet,replies', videoId=video_id, maxResults=100,
-                        textFormat='plainText', pageToken=response['nextPageToken']).execute()
-                except HttpError as e:
-                    print(e, video_id)
-                    break
-                comments_for_video = []
-                for item in response['items']:
-                    comment = item["snippet"]["topLevelComment"]
-                    # author = comment["snippet"]["authorDisplayName"]
-                    text = comment["snippet"]["textOriginal"]
-                    comments_for_video.append(text)
-                    if 'replies' in item:
-                        replies = item['replies']['comments']
-                        for reply in replies:
-                            text = reply["snippet"]["textOriginal"]
-                            comments_for_video.append(text)
-                comments[video_id].extend(comments_for_video)
-                if max_comments_per_video is not None:
-                    if len(comments[video_id]) >= max_comments_per_video:
-                        comments[video_id] = comments[video_id][:max_comments_per_video]
-                        break
+            if search_terms is None:
+                comments[video_id] = self.get_comments(
+                    video_id=video_id, max_comments_per_video=max_comments_per_video, order=order)
+            else:
+                comment_dicts = []
+                for term in search_terms:
+                    comment_dicts.append(self.get_comments(
+                        video_id=video_id, max_comments_per_video=max_comments_per_video,
+                        order=order, search_term=term, return_ids_dict=True))
+                concated_comments_dict = dict()
+                for d in comment_dicts:
+                    concated_comments_dict.update(d)
+                    print(len(d))
+                comments[video_id] = concated_comments_dict.values()
+
             if verbose:
                 print('{}. {} comments were retrieved for video id: {}'.format(i, len(comments[video_id]), video_id))
         if return_list:
@@ -127,7 +161,9 @@ class YouTubeRetriever:
         else:
             return comments
 
-    # NEEDS fixes to return ids
+
+
+    # NEEDS fixes
     # User might have private subscription list so this request may throw error
     # def get_subscriptions_list_by_id(self, channel_ids, **kwargs):
     #     # https://developers.google.com/youtube/v3/docs/subscriptions/list
